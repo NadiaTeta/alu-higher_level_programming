ab693dcbe88193cf89ad6ef951ff902453d9da72
NadiaTeta <n.teta2@alustudent.com>
2023-11-30 22:41:59 -0800

    create

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,8 +1,8 @@
 -- 1-create_database_if_missing.sql
--- Task: create the database hbtn_Oc_O if it doesn't already exist
+-- Task: create the database hbtn_0c_0 if it doesn't already exist
 
 -- Attempt to create the databases; ignore errors if it already exists
-CREATE DATABASE IF NOT EXISTS hbtn_Oc_O;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
--- Switch to the hbtn_Oc_O database
-USE hbtn_Oc_O;
+-- Switch to the hbtn_0c_0 database
+USE hbtn_0c_0;
